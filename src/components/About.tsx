2baaa1ba80b1a9818123c3864da0ec519bd26bde
Jhojan Sneider <jojangotam@gmail.com>
2025-10-23 15:52:49 -0500

    fix: Revert to direct public path for profile image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-export function About() {
+export const About = () => {
   return (
     <section
       id="sobre-mi"
